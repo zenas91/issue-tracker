de50e3dd65e12d1f40addeeddeb639f7cef9dfc2
Chukwuebuka Uchechukwu <zenaszone@gmail.com>
2026-05-05 09:15:48 +0200

    Added Markdown Editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,12 +1,19 @@
 "use client";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+import { Button, Skeleton, TextField } from "@radix-ui/themes";
+//import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+  loading: () => <Skeleton height="400px" />,
+});
 
 const NewIssuePage = () => {
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="Title" />
-      <TextArea placeholder="Description" />
+      <SimpleMDE placeholder="Description" />
       <Button>Submit New Issue</Button>
     </div>
   );
