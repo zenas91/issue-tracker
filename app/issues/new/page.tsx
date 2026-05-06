961302c40fe13dd1ba0a48503731cba91e4df5f7
Chukwuebuka Uchechukwu <zenaszone@gmail.com>
2026-05-06 12:15:18 +0200

    Code Cleanup

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,15 @@
 "use client";
-import { Button, Callout, Skeleton, Text, TextField } from "@radix-ui/themes";
-import { useForm, Controller } from "react-hook-form";
+import { ErrorMessage, Spinner } from "@/app/components";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Callout, Skeleton, TextField } from "@radix-ui/themes";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import dynamic from "next/dynamic";
-import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/validationSchemas";
+import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
