90748a75375757006c034f3c96d95fda490172bd
Chukwuebuka Uchechukwu <zenaszone@gmail.com>
2026-05-07 11:33:57 +0200

    Implemented Issue Delete API

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -28,3 +28,23 @@ export async function PATCH(
 
   return NextResponse.json(updatedIssue);
 }
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const { id } = await params;
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(id) },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: "Issue not found" }, { status: 404 });
+
+  await prisma.issue.delete({
+    where: { id: issue.id },
+  });
+
+  return NextResponse.json({});
+}
