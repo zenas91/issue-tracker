89fa8b409195f1624639ee08f176f9d06ecb6c46
Chukwuebuka Uchechukwu <zenaszone@gmail.com>
2026-05-08 18:50:53 +0200

    Adjusted NavBar Layout

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 import { signOut, useSession } from "./lib/auth_client";
-import { Box } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
@@ -22,34 +22,40 @@ const NavBar = () => {
     { label: "Issues", href: "/issues" },
   ];
   return (
-    <nav className="flex space-x-6 border-b px-5 mb-5 h-14 items-center">
-      <Link href="/">
-        <AiFillBug />
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              className={classNames({
-                "text-zinc-900": link.href === currentPath,
-                "text-zinc-500": link.href !== currentPath,
-                "hover:text-zinc-800 transition-colors": true,
-              })}
-              href={link.href}
-            >
-              {link.label}
+    <nav className="border-b px-5 mb-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <AiFillBug />
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {session && (
-          <Link href="/signin" onClick={handleSignOut}>
-            Sign Out
-          </Link>
-        )}
-        {!session && <Link href="/signin">Sign In</Link>}
-      </Box>
+            <ul className="flex space-x-4">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    className={classNames({
+                      "text-zinc-900": link.href === currentPath,
+                      "text-zinc-500": link.href !== currentPath,
+                      "hover:text-zinc-800 transition-colors": true,
+                    })}
+                    href={link.href}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {session && (
+              <Link href="/signin" onClick={handleSignOut}>
+                Sign Out
+              </Link>
+            )}
+            {!session && <Link href="/signin">Sign In</Link>}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
