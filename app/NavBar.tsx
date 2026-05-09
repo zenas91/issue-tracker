bb488053514aa278ecb14d053637501a354c44f9
Chukwuebuka Uchechukwu <zenaszone@gmail.com>
2026-05-09 11:39:40 +0200

    Refactored NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -18,19 +18,7 @@ import {
 
 const NavBar = () => {
   const currentPath = usePathname();
-  const { data: session } = useSession();
-  const router = useRouter();
 
-  console.log(session);
-  const handleSignOut = async () => {
-    await signOut();
-    router.push(currentPath);
-  };
-
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" },
-  ];
   return (
     <nav className="border-b px-5 mb-5 py-3">
       <Container>
@@ -39,76 +27,104 @@ const NavBar = () => {
             <Link href="/">
               <AiFillBug />
             </Link>
-            <ul className="flex space-x-4">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    className={classNames({
-                      "text-zinc-900": link.href === currentPath,
-                      "text-zinc-500": link.href !== currentPath,
-                      "hover:text-zinc-800 transition-colors": true,
-                    })}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks currentPath={currentPath} />
           </Flex>
-          <Box>
-            {session && (
-              <AlertDialog.Root>
-                <DropdownMenu.Root>
-                  <DropdownMenu.Trigger>
-                    <Button variant="ghost" className="p-0! border-0">
-                      <Avatar
-                        src={session.user.image!}
-                        fallback="?"
-                        size="2"
-                        radius="full"
-                        className="cursor-pointer"
-                      />
-                    </Button>
-                  </DropdownMenu.Trigger>
-                  <DropdownMenu.Content>
-                    <DropdownMenu.Label>
-                      <Text size="2">{session.user.email}</Text>
-                    </DropdownMenu.Label>
-                    <DropdownMenu.Separator />
-                    <AlertDialog.Trigger>
-                      <DropdownMenu.Item>Sign Out</DropdownMenu.Item>
-                    </AlertDialog.Trigger>
-                  </DropdownMenu.Content>
-                </DropdownMenu.Root>
-
-                <AlertDialog.Content maxWidth="400px">
-                  <AlertDialog.Title>Sign Out</AlertDialog.Title>
-                  <AlertDialog.Description>
-                    Are you sure you want to sign out?
-                  </AlertDialog.Description>
-                  <Flex gap="3" justify="end" mt="4">
-                    <AlertDialog.Cancel>
-                      <Button variant="soft" color="gray">
-                        Cancel
-                      </Button>
-                    </AlertDialog.Cancel>
-                    <AlertDialog.Action>
-                      <Button color="red" onClick={handleSignOut}>
-                        Sign Out
-                      </Button>
-                    </AlertDialog.Action>
-                  </Flex>
-                </AlertDialog.Content>
-              </AlertDialog.Root>
-            )}
-            {!session && (
-              <Link href={`/signin?callbackURL=${currentPath}`}>Sign In</Link>
-            )}
-          </Box>
+          <AuthStatus currentPath={currentPath} />
         </Flex>
       </Container>
     </nav>
+  );
+};
+
+const NavLinks = ({ currentPath }: { currentPath: string }) => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <ul className="flex space-x-4">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classNames({
+              "nav-link": true,
+              "text-zinc-900!": link.href === currentPath,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const AuthStatus = ({ currentPath }: { currentPath: string }) => {
+  const { data: session, isPending } = useSession();
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    await signOut();
+    router.push(currentPath);
+  };
+
+  if (isPending) return null;
+
+  if (!session)
+    return (
+      <Link className="nav-link" href={`/signin?callbackURL=${currentPath}`}>
+        Sign In
+      </Link>
+    );
+
+  return (
+    <Box>
+      <AlertDialog.Root>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Button variant="ghost" className="p-0! border-0">
+              <Avatar
+                src={session.user.image!}
+                fallback="?"
+                size="2"
+                radius="full"
+                className="cursor-pointer"
+              />
+            </Button>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Label>
+              <Text size="2">{session.user.email}</Text>
+            </DropdownMenu.Label>
+            <DropdownMenu.Separator />
+            <AlertDialog.Trigger>
+              <DropdownMenu.Item>Sign Out</DropdownMenu.Item>
+            </AlertDialog.Trigger>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+
+        <AlertDialog.Content maxWidth="400px">
+          <AlertDialog.Title>Sign Out</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to sign out?
+          </AlertDialog.Description>
+          <Flex gap="3" justify="end" mt="4">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red" onClick={handleSignOut}>
+                Sign Out
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </Box>
   );
 };
 
