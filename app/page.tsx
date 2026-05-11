91ba7430ec3d92c9f1c0f7692591c1c793499da4
Chukwuebuka Uchechukwu <zenaszone@gmail.com>
2026-05-11 10:42:43 +0200

    Designed Pagination Layout Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
+import Pagination from "./components/Pagination";
 
 export default function Home() {
-  return <div>Hello World</div>;
+  return <Pagination itemCount={100} pageSize={10} currentPage={10} />;
 }
