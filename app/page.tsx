3282ef5c1ada2d6f5700a09d797d2fc3db0c2b77
Chukwuebuka Uchechukwu <zenaszone@gmail.com>
2026-05-11 14:41:49 +0200

    Implemented Latest Issues Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-import Image from "next/image";
-import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
-interface Props {
-  searchParams: { page: string };
-}
-export default async function Home({ searchParams }: Props) {
-  const { page } = await searchParams;
-  return (
-    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(page)} />
-  );
+export default async function Home() {
+  return <LatestIssues />;
 }
